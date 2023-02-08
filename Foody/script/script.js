b493fa64ac92928c3f2cd4f647a794cb4df10cc1
Naseer Ahmed <121551293+NaseerAhmed22@users.noreply.github.com>
2023-02-08 09:06:14 -0800

    Add files via upload

---
+++ Foody/script/script.js
@@ -0,0 +1,7 @@
+const mobileMenu = document.querySelector(".mobile-menu");
+mobileMenu.addEventListener("click" ,() =>{
+    document.querySelector(".nav-links").classList.toggle("show");
+});
+
+
+
